70db7f341b740b1dbe7ffe7977a4100880c3a0cc
Kaikaio <1035759181@qq.com>
2025-07-23 01:54:24 +0800

    feat: 首次修改

--- App.tsx
+++ App.tsx
@@ -6,13 +6,14 @@
  */
 
 import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { StatusBar, StyleSheet, useColorScheme, Text, View } from 'react-native';
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
 
   return (
     <View style={styles.container}>
+      <Text>Kaikaio ReactNative 4 Success!!</Text>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <NewAppScreen templateFileName="App.tsx" />
     </View>
